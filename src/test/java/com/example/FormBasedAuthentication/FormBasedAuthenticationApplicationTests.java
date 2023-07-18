843dd1bace148d62308a9125942300649dfea663
Olaki Edwin <olakiedwin@gmail.com>
2023-07-18 11:48:45 +0300

    Configuration file

---
+++ src/test/java/com/example/FormBasedAuthentication/FormBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FormBasedAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBasedAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
